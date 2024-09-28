1b008d71da3437426b362dde667b8e7646b9ee30
r0227n <developryo@gmail.com>
2024-09-28 20:35:01 +0900

    add: 同じlist場合、callback block

--- lib/widgets/drag_list_view.dart
+++ lib/widgets/drag_list_view.dart
@@ -1,27 +1,57 @@
 import 'package:flutter/material.dart';
 
+/// A typedef representing the signature of a callback function that is called when a drag target accepts a draggable item with additional details.
+///
+/// The [fromListId] parameter represents the ID of the list from which the item is being dragged.
+/// The [toListId] parameter represents the ID of the list to which the item is being dragged.
+/// The [itemId] parameter represents the ID of the item being dragged.
+typedef DragTargetAcceptWithDetailsValue = ({
+  int? fromListId,
+  int? toListId,
+  int itemId,
+});
+
 class DragListItem {
   const DragListItem({
-    required this.id,
+    this.listId,
+    required this.itemId,
     this.draggingChild,
     required this.child,
   });
 
+  /// The unique identifier for the drag list
+  final int? listId;
+
   /// The unique identifier for the drag list item
-  final int id;
+  final int itemId;
 
   /// The widget to display when the item is being dragged
   final Widget? draggingChild;
 
   /// The main widget to display for the item
   final Widget child;
+
+  DragListItem copyWith({
+    int? listId,
+    int? itemId,
+    Widget? draggingChild,
+    Widget? child,
+  }) {
+    return DragListItem(
+      listId: listId ?? this.listId,
+      itemId: itemId ?? this.itemId,
+      draggingChild: draggingChild ?? this.draggingChild,
+      child: child ?? this.child,
+    );
+  }
 }
 
 /// A widget that displays a list of draggable items.
 class DragListView extends StatelessWidget {
-  const DragListView({
+  DragListView({
     super.key,
-    required this.children,
+    this.id,
+    required List<DragListItem> items,
     this.feedback = const SizedBox.square(
       dimension: 100,
       child: ColoredBox(
@@ -34,7 +64,10 @@ class DragListView extends StatelessWidget {
     ),
     this.draggingChild,
     required this.onAcceptWithDetails,
-  });
+  }) : children = items.map((e) => e.copyWith(listId: id)).toList();
+
+  /// The unique identifier for the drag list
+  final int? id;
 
   /// The list of draggable items to display.
   final List<DragListItem> children;
@@ -46,11 +79,11 @@ class DragListView extends StatelessWidget {
   final Widget? draggingChild;
 
   /// Callback function that is called when an item is accepted by a [DragTarget].
-  final DragTargetAcceptWithDetails<int> onAcceptWithDetails;
+  final void Function(DragTargetAcceptWithDetailsValue) onAcceptWithDetails;
 
   @override
   Widget build(BuildContext context) {
-    return DragTarget<int>(
+    return DragTarget<DragListItem>(
       builder: (
         BuildContext context,
         List<dynamic> accepted,
@@ -59,8 +92,8 @@ class DragListView extends StatelessWidget {
         return ListView.builder(
           itemCount: children.length,
           itemBuilder: (context, index) {
-            return Draggable<int>(
-              data: children[index].id,
+            return Draggable<DragListItem>(
+              data: children[index],
               feedback: feedback,
               childWhenDragging: children[index].draggingChild ??
                   draggingChild ??
@@ -73,7 +106,34 @@ class DragListView extends StatelessWidget {
           },
         );
       },
-      onAcceptWithDetails: onAcceptWithDetails,
+      onAcceptWithDetails: (details) {
+        // If the item is being dragged to the same list, do nothing
+        if (details.data.listId == id) {
+          return;
+        }
+
+        onAcceptWithDetails((
+          fromListId: details.data.listId,
+          toListId: id,
+          itemId: details.data.itemId,
+        ));
+      },
+    );
+  }
+
+  DragListView copyWith({
+    int? id,
+    List<DragListItem>? children,
+    Widget? feedback,
+    Widget? draggingChild,
+    void Function(DragTargetAcceptWithDetailsValue)? onAcceptWithDetails,
+  }) {
+    return DragListView(
+      id: id ?? this.id,
+      items: children ?? this.children,
+      feedback: feedback ?? this.feedback,
+      draggingChild: draggingChild ?? this.draggingChild,
+      onAcceptWithDetails: onAcceptWithDetails ?? this.onAcceptWithDetails,
     );
   }
 }

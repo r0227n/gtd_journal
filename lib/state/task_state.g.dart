e6e2af9a6177ffa2394ebbee8eb5223662cff61a
r0227n <developryo@gmail.com>
2024-09-29 16:24:34 +0900

    add: Task state AsyncNotifier

---
+++ lib/state/task_state.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'task_state.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$taskStateHash() => r'536e1ceba3213ef2bcac1a115a0981247749c57c';
+
+/// See also [TaskState].
+@ProviderFor(TaskState)
+final taskStateProvider =
+    AutoDisposeAsyncNotifierProvider<TaskState, List<Task>>.internal(
+  TaskState.new,
+  name: r'taskStateProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$taskStateHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$TaskState = AutoDisposeAsyncNotifier<List<Task>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
